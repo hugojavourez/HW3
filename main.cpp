93953d37d954abd1508ed97d47d7e50029ddb1c8
hugojavourez <javourezhugo77@gmail.com>
2024-11-18 00:47:31 -0500

    Volume and length functions

--- main.cpp
+++ main.cpp
@@ -1,27 +1,16 @@
 #include "grid.h"
+#include "math.h"
 
 #include <iostream>
 #include <vector>
 
 int main() {
-    std::string filename = "9x9.x"; // Replace with your file path
+    std::string filename = "9x9.x"; // Replace with the grid size wanted
     std::vector<double> xCoords, yCoords;
+    int n1, n2;
 
     // Read the coordinates from the file
-    readCoordinates(filename, xCoords, yCoords);
-
-    // Output the coordinates for verification
-    std::cout << "X Coordinates:" << std::endl;
-    for (double x : xCoords) {
-        std::cout << x << " ";
-    }
-    std::cout << std::endl;
-
-    std::cout << "Y Coordinates:" << std::endl;
-    for (double y : yCoords) {
-        std::cout << y << " ";
-    }
-    std::cout << std::endl;
+    readCoordinates(filename, xCoords, yCoords, n1, n2);
 
     return 0;
 }
---
+++ math.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void crossProduct(const double A[2], const double B[2], double result);
+void norm(const double A[2], double result);

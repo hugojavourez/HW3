cea4b7e803dc8c7685bf682b60e5843f3f458e26
hugojavourez <javourezhugo77@gmail.com>
2024-11-27 01:08:44 -0500

    Adding cellToFaces in the connectivity function (to be able to get the normal vector of a face forming a cell) AND Adding the BoundaryConditions function that take care of the boundary conditions (at initialization and at each time step)

--- solver.cpp
+++ solver.cpp
@@ -1,5 +1,6 @@
 #include "math.h"
 
+#include <string>
 #include <iostream>
 #include <vector>
 
@@ -18,7 +19,7 @@ void Initialization(int n, double MachNumber, double AoA, double fluidProperties
     // Calculate the total energy
     double totalEnergy = (fluidProperties[1] / ((fluidProperties[2] - 1)) + 0.5 * fluidProperties[0] * pow(velocityMagnitude,2));
 
-    // Calculate all vectors
+    // Loop over the cells
     for (int c = 0; c < cellNumber; c++) {
         // Calculate the fluid properties vector W
         W[3 * c] = fluidProperties[0];
@@ -28,6 +29,124 @@ void Initialization(int n, double MachNumber, double AoA, double fluidProperties
 
         // Calculate the residual vector R (initially set to zero)
         R[c] = 0;
+    }
+}
+
+void BoundaryConditions(const int n, const double MachNumber, const double AoA, double fluidProperties[5], const std::vector<int>& cellType, std::vector<int>& cellToFaces, std::vector<double>& xNormal, std::vector<double>& yNormal, std::vector<double>& W, std::vector<double>& R) {
+    int physicalCellIndex; // Index of the physical cell touching the boundary
+    int faceIndex; // Index of the face between the physical and the ghost cell
+    double normalVector[2]; // Normal vector of the face
+
+    double dotProductResult = 0; // Dot product of the farfield flow velocity and the normal vector
+
+    double soundSpeed; // Speed of sound
+
+    double flowVelocity[2]; // Flow velocity of the physical cell or the farfield state (in the else part of the loop)
+    double velocityMagnitude; // Magnitude of the flow velocity
+    double totalEnergy; // Total energy of the physical cell or the farfield state (in the else part of the loop)
+    double pPhysical; // Physical cell pressure
+    double pGhost, rhoGhost, uGhost, vGhost, EGhost; // Ghost cell properties
+
+    // Loop over the cells
+    for (int c = 0; c < cellType.size(); c++) {
+        // Check if the cell is a boundary cell (-1 if it is a wall, 1 if it is a farfield)
+        if (cellType[c] == -1) {
+            // Determine the index of the physical cell touching the boundary
+            if (cellType[c+n-1] == -1) { // The current cell is on the second layer of ghost cells
+                physicalCellIndex = c + (n - 1) * 2;
+            } else { // The current cell is on the first layer of ghost cells
+                physicalCellIndex = c + (n - 1);
+            }
+
+            // Determine the index of the face between the physical and the ghost cell
+            faceIndex = cellToFaces[4*physicalCellIndex];
+
+            // Calculate the flow velocity in the physical cell
+            flowVelocity[0] = W[3 * physicalCellIndex + 1] / W[3 * physicalCellIndex];
+            flowVelocity[1] = W[3 * physicalCellIndex + 2] / W[3 * physicalCellIndex];
+            velocityMagnitude = sqrt(pow(flowVelocity[0],2) + pow(flowVelocity[1],2));
+            // Calculate the flow velocity in the ghost cell
+            uGhost = flowVelocity[0] - 2*velocityMagnitude * xNormal[faceIndex]; 
+            vGhost = flowVelocity[1] - 2*velocityMagnitude * yNormal[faceIndex];
+            // Calculate the pressure and the total energy in the physical cell
+            pGhost = (fluidProperties[2] - 1) * ((W[3 * physicalCellIndex + 3] / W[3 * physicalCellIndex]) - 0.5 * W[3 * physicalCellIndex] * pow(velocityMagnitude,2));
+            EGhost = pGhost / ((fluidProperties[2] - 1)) + 0.5 * W[3 * physicalCellIndex] * (pow(uGhost,2) + pow(vGhost,2));
+
+            // Set the values of the ghost cell
+            W[3 * c] = W[3 * physicalCellIndex];
+            W[3 * c + 1] = W[3 * c] * uGhost;
+            W[3 * c + 2] = W[3 * c] * vGhost;
+            W[3 * c + 3] = W[3 * c] * EGhost;
+
+            // Setting the residual to zero
+            R[c] = 0;
+
+        } else if (cellType[c] == 1) {
+            // Determine the index of the physical cell touching the boundary
+            if (cellType[c-(n-1)] == -1) { // The current cell is on the second layer of ghost cells
+                physicalCellIndex = c - (n - 1) * 2;
+            } else { // The current cell is on the first layer of ghost cells
+                physicalCellIndex = c - (n - 1);
+            }
+
+            // Determine the index of the face between the physical and the ghost cell
+            faceIndex = cellToFaces[4*physicalCellIndex];
+
+            // Calculate the flow velocity vector and the normal vector of the face
+            flowVelocity[0] = MachNumber * cos(AoA);
+            flowVelocity[1] = MachNumber * sin(AoA);
+            velocityMagnitude = sqrt(pow(flowVelocity[0],2) + pow(flowVelocity[1],2));
+            normalVector[0] = xNormal[c];
+            normalVector[1] = yNormal[c];
+
+            // Determine if the flow is entering or leaving the domain
+            dotProduct(flowVelocity, normalVector, dotProductResult);
+
+            // Determine farfield boundary type
+            if (MachNumber < 1.0) {
+                if (dotProductResult < 0.0) {
+                    // Subsonic inflow
+                    soundSpeed = sqrt(fluidProperties[2] * fluidProperties[3] * fluidProperties[4]);
+                    pPhysical = (fluidProperties[2] - 1) * ((W[3 * physicalCellIndex + 3] / W[3 * physicalCellIndex]) - 0.5 * W[3 * physicalCellIndex] * pow(sqrt(pow(W[3 * physicalCellIndex + 1],2) + pow(W[3 * physicalCellIndex + 2],2)) / W[3 * physicalCellIndex],2));
+                    pGhost = 0.5 * (fluidProperties[1] + pPhysical - W[3 * physicalCellIndex] *soundSpeed * ((flowVelocity[0] - (W[3 * physicalCellIndex + 1] / W[3 * physicalCellIndex])) * xNormal[faceIndex] + (flowVelocity[1] - (W[3 * physicalCellIndex + 2] / W[3 * physicalCellIndex])) * yNormal[faceIndex]));
+                    rhoGhost = W[3 * physicalCellIndex] + (pGhost - pPhysical) / pow(soundSpeed,2);
+                    uGhost = (W[3 * physicalCellIndex + 1] / W[3 * physicalCellIndex]) - xNormal[faceIndex] * (pPhysical - pGhost) / (W[3 * physicalCellIndex] * soundSpeed);
+                    vGhost = (W[3 * physicalCellIndex + 2] / W[3 * physicalCellIndex]) - yNormal[faceIndex] * (pPhysical - pGhost) / (W[3 * physicalCellIndex] * soundSpeed);
+                    EGhost = pGhost / ((fluidProperties[2] - 1)) + 0.5 * rhoGhost * (pow(uGhost,2) + pow(vGhost,2));
+                } else {
+                    // Subsonic outflow
+                    soundSpeed = sqrt(fluidProperties[2] * fluidProperties[3] * fluidProperties[4]);
+                    pPhysical = (fluidProperties[2] - 1) * ((W[3 * physicalCellIndex + 3] / W[3 * physicalCellIndex]) - 0.5 * W[3 * physicalCellIndex] * pow(sqrt(pow(W[3 * physicalCellIndex + 1],2) + pow(W[3 * physicalCellIndex + 2],2)) / W[3 * physicalCellIndex],2));
+                    pGhost = fluidProperties[1];
+                    rhoGhost = W[3 * physicalCellIndex] + (pGhost - pPhysical) / pow(soundSpeed,2);
+                    uGhost = (W[3 * physicalCellIndex + 1] / W[3 * physicalCellIndex]) + xNormal[faceIndex] * (pPhysical - pGhost) / (W[3 * physicalCellIndex] * soundSpeed);
+                    vGhost = (W[3 * physicalCellIndex + 2] / W[3 * physicalCellIndex]) + yNormal[faceIndex] * (pPhysical - pGhost) / (W[3 * physicalCellIndex] * soundSpeed);
+                    EGhost = pGhost / ((fluidProperties[2] - 1)) + 0.5 * rhoGhost * (pow(uGhost,2) + pow(vGhost,2));
+                }
+                W[3 * c] = rhoGhost;
+                W[3 * c + 1] = rhoGhost * uGhost;
+                W[3 * c + 2] = rhoGhost * vGhost;
+                W[3 * c + 3] = rhoGhost * EGhost;
+            } else {
+                if (dotProductResult < 0.0) {
+                    // Supersonic inflow (the state is set to the farfield state)
+                    totalEnergy = (fluidProperties[1] / ((fluidProperties[2] - 1)) + 0.5 * fluidProperties[0] * pow(velocityMagnitude,2));
+                    W[3 * c] = fluidProperties[0];
+                    W[3 * c + 1] = fluidProperties[0] * flowVelocity[0];
+                    W[3 * c + 2] = fluidProperties[0] * flowVelocity[1];
+                    W[3 * c + 3] = fluidProperties[0] * totalEnergy;
+                } else {
+                    // Supersonic outflow (the state is set to the physical state)
+                    W[3 * c] = W[3 * physicalCellIndex];
+                    W[3 * c + 1] = W[3 * physicalCellIndex + 1];
+                    W[3 * c + 2] = W[3 * physicalCellIndex + 2];
+                    W[3 * c + 3] = W[3 * physicalCellIndex + 3];
+                }
+            }
+
+            // Setting the residual to zero
+            R[c] = 0;
+        }
     }
 }
 
--- solver.h
+++ solver.h
@@ -8,5 +8,7 @@
 void Initialization(int n, double MachNumber, double AoA, double fluidProperties[5], int cellNumber, std::vector<int>& faceToCellsLeft, std::vector<int>& faceToCellsRight, std::vector<double>& xNormal, std::vector<double>& yNormal, std::vector<double>& W, std::vector<double>& R);
 
 // Boundary condition functions
-void BoundaryConditions(const int n, const std::vector<int>& faceType, const std::vector<int>& cellType); // Not finished
-void Solve();
+void BoundaryConditions(const int n, const double MachNumber, const double AoA, double fluidProperties[5], const std::vector<int>& cellType, std::vector<int>& cellToFaces, std::vector<double>& xNormal, std::vector<double>& yNormal, std::vector<double>& W, std::vector<double>& R);
+
+// Calculate the flux
+void Calculateflux(int n, double MachNumber, double AoA, double fluidProperties[5], int faceNumber, int cellNumber, std::vector<int>& faceToCellsLeft, std::vector<int>& faceToCellsRight, std::vector<double>& xNormal, std::vector<double>& yNormal, std::vector<double>& W, std::vector<double>& Fc, std::vector<double>& R);
